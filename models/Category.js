ac3963b6bb81492a389ed1076abbeb1395c33add
cpm-128 <101371032+cpm-128@users.noreply.github.com>
2022-07-09 19:41:27 -0400

    Products model

--- models/Category.js
+++ models/Category.js
@@ -4,11 +4,27 @@ const sequelize = require('../config/connection.js');
 
 class Category extends Model {}
 
+// define the table
 Category.init(
-  {
-    // define columns
+  {  // ================================
+     // TABLE COLUMN DEFINITIONS GO HERE
+     // defining the Post schema
+     // ================================
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
-  {
+  {  // ====================================
+     // TABLE CONFIGURATIONS OPTIONS GO HERE
+     // configure the metadata
+     //=====================================
     sequelize,
     timestamps: false,
     freezeTableName: true,
