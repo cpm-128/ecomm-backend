7894cbcea3be8af743cca4dc2d841221d12bc80c
cpm-128 <101371032+cpm-128@users.noreply.github.com>
2022-07-09 20:24:08 -0400

    model associations

--- models/index.js
+++ models/index.js
@@ -4,13 +4,34 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+// ============
+// ASSOCIATIONS
+// ============
+
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  // Product belongs to Category through what fk?
+  foreignKey: 'category_id'
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  // as: 'TBD OPTIONAL LABEL',
+  foreignKey: 'product_id'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  // as: 'TBD OPTIONAL LABEL',
+  foreignKey: 'tag_id'
+});
 
 module.exports = {
   Product,
