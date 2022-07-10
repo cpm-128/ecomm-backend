50658bce06f129ee14e55326d944a0694b2176e5
cpm-128 <101371032+cpm-128@users.noreply.github.com>
2022-07-09 23:30:54 -0400

    trying to syn the Sequelize models to the MySql db on the server start

--- config/connection.js
+++ config/connection.js
@@ -2,9 +2,31 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
+// ===========
+// from module
+// ===========
+
+//create connection to our database
+// let sequelize;
+
+// if (process.env.JAWSDB_URL) {
+//     sequelize = new Sequelize(process.env.JAWSDB_URL);
+// } else {
+//     sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+//         host: 'localhost',
+//         dialect: 'mysql',
+//         dialectOptions: {
+//           decimalNumbers: true,
+//         }
+//     });
+// };
+
+// =====================
+// provided started code
+// =====================
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
       host: 'localhost',
       dialect: 'mysql',
       dialectOptions: {
