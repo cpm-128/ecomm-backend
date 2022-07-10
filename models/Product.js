e1ad93773a6ee56431db3c6bb8ea3eb372fba513
cpm-128 <101371032+cpm-128@users.noreply.github.com>
2022-07-09 20:02:18 -0400

    productTag model

--- models/Product.js
+++ models/Product.js
@@ -55,7 +55,7 @@ Product.init(
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: "product",
+    modelName: 'product',
   }
 );
 
--- models/ProductTag.js
+++ models/ProductTag.js
@@ -5,10 +5,37 @@ const sequelize = require('../config/connection');
 class ProductTag extends Model {}
 
 ProductTag.init(
-  {
-    // define columns
+  { // ================================
+    // TABLE COLUMN DEFINITIONS GO HERE
+    // defining the Post schema
+    // ================================
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      // FOREIGN KEY LINK
+      references: {
+        model: 'product',
+        key: 'id'
+      }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      // FOREGIN KEY LINK
+      references: {
+        model: 'tag',
+        key: 'id'
+      }
+    }
   },
-  {
+  { // ====================================
+    // TABLE CONFIGURATIONS OPTIONS GO HERE
+    // configure the metadata
+    //=====================================
     sequelize,
     timestamps: false,
     freezeTableName: true,
